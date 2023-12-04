b020e329515708367b6db8e39324f40a357f8389
OverdueWeevil2 <67264530+OverdueWeevil2@users.noreply.github.com>
2023-12-04 16:05:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/sh
 pkg install x11-repo -y
 pkg install tigervnc xfce4 xfce4-goodies firefox -y
+mkdir -p $HOME/.vnc
 echo "#!/data/data/com.termux/files/usr/bin/sh" >> $HOME/.vnc/xstartup
 echo 'export DISPLAY=":1"' >> $HOME/.vnc/xstartup
 echo "xfce4-session &" >> $HOME/.vnc/xstartup
-vncserver
 echo "Type vncserver -kill :1 to stop, type vncserver to start."

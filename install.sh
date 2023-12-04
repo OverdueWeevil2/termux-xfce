ead206054bb391c540735b6293fc9ffc0d288417
OverdueWeevil2 <67264530+OverdueWeevil2@users.noreply.github.com>
2023-12-04 16:20:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ mkdir -p $HOME/.vnc
 echo "#!/data/data/com.termux/files/usr/bin/sh" >> $HOME/.vnc/xstartup
 echo 'export DISPLAY=":1"' >> $HOME/.vnc/xstartup
 echo "xfce4-session &" >> $HOME/.vnc/xstartup
+chmod a+x $HOME/.vnc/xstartup
 echo "Type vncserver to start VNC Server. IP: localhost:5901"

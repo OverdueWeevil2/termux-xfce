515dba79844e8d068c4add1996a144c21936dcdb
OverdueWeevil2 <67264530+OverdueWeevil2@users.noreply.github.com>
2024-01-31 19:25:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ pkg install tigervnc xfce4 xfce4-goodies firefox -y
 mkdir -p $HOME/.vnc
 echo "#!/data/data/com.termux/files/usr/bin/sh" >> $HOME/.vnc/xstartup
 echo 'export DISPLAY=":1"' >> $HOME/.vnc/xstartup
-echo "startxfce4 &" >> $HOME/.vnc/xstartup
+echo "exec startxfce4" >> $HOME/.vnc/xstartup
 chmod a+x $HOME/.vnc/xstartup
 echo "Type vncserver to start VNC Server. IP: localhost:5901"
 vncserver
